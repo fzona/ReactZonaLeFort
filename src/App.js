eb5ddfe92c59b8c0d9a23ef88d1d88e083316071
Federico Zona Le Fort <fzonalefort@gmail.com>
2022-06-04 03:44:27 -0300

    Agegue un contador de articulos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import ItemCount from './components/ItemCount';
 import ItemListContainer from './components/ItemListContainer';
 import Navbar from './components/Navbar';
 
 
 function App() {
+    const onAdd = (count) => {
+    alert (`Agregaste ${count} productos a tu carrito`);
+  }
+
+
   return (
     <>
     <Navbar/>
     <ItemListContainer greeting={"Bienvenido a la mejor página de camisetas del mundo, por ahora no tenemos ninguna :P"}/> 
+    <ItemCount inicial={1} stock={7} onAdd={onAdd}/>
     </>
    );
 }
---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export default function ItemCount({inicial, stock, onAdd}) {
+
+    const [count,setCount] = useState(inicial);
+    
+    const sumar = () => {
+        count < stock ? setCount(count + 1):alert("No puedes agregar más productos");
+    }
+
+    const restar = () => {
+        count > inicial ? setCount(count - 1):alert("No puedes quitar más productos");
+    }
+
+
+
+  return (
+     <>
+        <h2>{count}</h2>
+        <button onClick={sumar}>+</button>
+        <button onClick={restar}>-</button>
+        <button onClick={()=>onAdd(count)}>Agregar al carrito</button>
+     </>   
+  )
+}
